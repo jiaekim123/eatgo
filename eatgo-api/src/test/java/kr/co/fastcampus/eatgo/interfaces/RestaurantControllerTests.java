7f2ff0806652171143eeccd34fecd9f2ca6d7dce
jiae <jiae_kim@tmax.co.kr>
2020-04-07 19:39:00 +0900

    [fix] add mockbean and given value

--- eatgo-api/src/test/java/kr/co/fastcampus/eatgo/interfaces/RestaurantControllerTests.java
+++ eatgo-api/src/test/java/kr/co/fastcampus/eatgo/interfaces/RestaurantControllerTests.java
@@ -3,60 +3,53 @@ package kr.co.fastcampus.eatgo.interfaces;
 import kr.co.fastcampus.eatgo.application.RestaurantService;
 import kr.co.fastcampus.eatgo.domain.*;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.hamcrest.core.StringContains.containsString;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@ExtendWith(SpringExtension.class)
-//@SpringJUnitConfig
+@SpringJUnitConfig
 @WebMvcTest(RestaurantController.class)
 public class RestaurantControllerTests {
-//
-//    @Configuration
-//    static class Config{
-//        @Bean
-//        RestaurantController restaurantController(){
-//            return new RestaurantController();
-//        }
-//        @Bean
-//        RestaurantRepository restaurantRepository(){
-//            return new RestaurantRepositoryImpl();
-//        }
-//        @Bean
-//        RestaurantRepository restaurantRepository(){
-//            return new RestaurantRepositoryImpl();
-//        }
-//
-//    }
+
+    @Configuration
+    static class Config{
+        @Bean
+        RestaurantController restaurantController(){
+            return new RestaurantController();
+        }
+        @Bean
+        RestaurantRepository restaurantRepository(){
+            return new RestaurantRepositoryImpl();
+        }
+        @Bean
+        MenuItemRepository menuItemRepository(){
+            return new MenuItemRepositoryImpl();
+        }
+
+    }
     @Autowired
     private MockMvc mvc;
 
-//    @MockBean
-//    private RestaurantService restaurantService;
-
-    @SpyBean(RestaurantRepositoryImpl.class)
-    private RestaurantRepository restaurantRepository;
-
-    @SpyBean(MenuItemRepositoryImpl.class)
-    private MenuItemRepository menuItemRepository;
-
-    @SpyBean(RestaurantService.class)
+    @MockBean
     private RestaurantService restaurantService;
-
     @Test
     public void list() throws Exception {
+        List<Restaurant> restaurants = new ArrayList<>();
+        restaurants.add(new Restaurant(1004L, "Bob zip", "Seoul"));
+        given(restaurantService.getRestaurants()).willReturn(restaurants);
         mvc.perform(get("/restaurants"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(
@@ -67,6 +60,14 @@ public class RestaurantControllerTests {
 
     @Test
     public void detail() throws Exception {
+        List<Restaurant> restaurants = new ArrayList<>();
+        Restaurant restaurant1 = new Restaurant(1004L, "Bob zip", "Seoul");
+        restaurant1.addMenuItem(new MenuItem("Kimchi"));
+        Restaurant restaurant2 = new Restaurant(2020L, "Cyber Food", "Seoul");
+
+        given(restaurantService.getRestaurant(1004L)).willReturn(restaurant1);
+        given(restaurantService.getRestaurant(2020L)).willReturn(restaurant2);
+
         mvc.perform(get("/restaurants/1004"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(
